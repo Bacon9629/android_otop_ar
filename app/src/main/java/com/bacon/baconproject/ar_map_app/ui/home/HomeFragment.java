31a514e02d83d5d9cba8217c432cccf2cf4ef1bf
Bacon9629 <qwe43213652@gmail.com>
2022-10-10 21:32:42 +0800

    map ok, news begin

--- app/src/main/java/com/bacon/baconproject/ar_map_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/bacon/baconproject/ar_map_app/ui/home/HomeFragment.java
@@ -1,20 +1,28 @@
 package com.bacon.baconproject.ar_map_app.ui.home;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bacon.baconproject.ar_map_app.R;
 import com.bacon.baconproject.ar_map_app.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
+    private Context context;
+
     private FragmentHomeBinding binding;
+    private ImageView map_point_img;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,9 +31,19 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        context = getActivity();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        map_point_img = root.findViewById(R.id.map_point);
+        map_point_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                View v = LayoutInflater.from(context).inflate(R.layout.alert_map, null);
+                builder.setView(v);
+                builder.show().getWindow().setBackgroundDrawable(new ColorDrawable(0));
+            }
+        });
+
         return root;
     }
 
@@ -34,4 +52,5 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 }
